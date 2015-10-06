bf8c2b6f44fa8d2a196eaa6e4a0c3b4504966227
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2015-10-06 10:32:34 +0300

    Initial commit

---
+++ app/src/test/java/com/kurkov/p0981_servicebindinglocal/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.kurkov.p0981_servicebindinglocal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
